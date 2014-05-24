bbdb37d83d626f0fa5bbe5cb19e98a824284e7dd
Pix <pix@test.com>
2014-05-24 14:36:17 +0700

    add Random and RandomTest class

---
+++ Random.php
@@ -0,0 +1,20 @@
+<?php
+
+class Random
+{
+    public function getPattern()
+    {
+        return rand(1, 2);
+    }
+
+    public function getOperand()
+    {
+        return rand(1, 9);
+    }
+
+    public function getOperator()
+    {
+        return rand(1, 3);
+    }
+}
+?>
---
+++ RandomTest.php
@@ -0,0 +1,25 @@
+<?php
+require_once "Random.php";
+
+class RandomTest extends PHPUnit_Framework_TestCase
+{
+    public function test_randomPattern_return_1_or_2()
+    {
+        $random = new Random();
+        $this->assertContains($random->getPattern(), array(1, 2));
+    }
+
+    public function test_randomOperand_return_1_to_9()
+    {
+        $random = new Random();
+        $this->assertContains($random->getOperand(), array(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    }
+
+    public function test_randomOperator_return_1_to_3()
+    {
+        $random = new Random();
+        $this->assertContains($random->getOperator(), array(1, 2, 3));
+    }
+}
+
+?>
